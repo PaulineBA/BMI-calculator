ad21822c2905a6751e50748858aa85e249a701cd
pauline_barnades <barnades.pauline@gmail.com>
2020-04-02 09:26:55 +0200

    doesn't pass test as calculateMetric not defined

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,7 +1,7 @@
 const  calculateBMI = () => {
 	let  weightInput = document.getElementById("weight")
 	let  heightInput = document.getElementById("height")
-}
+
 let  results = calculateMetricBMI(weightInput.value, heightInput.value)
 
 let  displayResultsElement = document.getElementById('results')
